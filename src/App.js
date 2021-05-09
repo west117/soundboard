a9aa01242edaf0c5678498d3a64e6f6aaf26030e
David <code@west117.co.uk>
2021-05-09 18:43:07 +0100

    Well it works...

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import useSound from "use-sound";
+import useMeasure from "react-use-measure";
+import * as R from "ramda";
+import { animated, useSpring } from "react-spring";
+
+import { Sounds } from "./sounds";
 
 function App() {
+  const sorted = useMemo(() => R.sortBy(R.prop("name"), Sounds), []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SoundBoard>
+        {sorted.map((s, idx) => (
+          <SoundButtonLayout key={`sound-${idx}}`}>
+            <SoundButton {...s} />
+          </SoundButtonLayout>
+        ))}
+      </SoundBoard>
     </div>
   );
 }
+
+const SoundBoard = ({ children }) => (
+  <div className="SoundBoard">{children}</div>
+);
+
+const SoundButtonLayout = ({ children }) => (
+  <div className="SoundButtonLayout">{children}</div>
+);
+
+const SoundButton = ({ name, src }) => {
+  const [, setCount] = useState(0);
+  const [play, { stop, isPlaying, duration }] = useSound(src);
+  const [ref, bounds] = useMeasure();
+  const styles = useSpring({
+    config: { duration },
+    from: { width: 0 },
+    to: { width: bounds.width },
+    reset: isPlaying,
+  });
+
+  const onClick = useCallback(() => {
+    if (isPlaying) {
+      // Hack to make the animation restart. Shrug.
+      setCount((prev) => prev + 1);
+      stop();
+    }
+
+    play();
+  }, [stop, play, isPlaying]);
+
+  useEffect(() => stop, [stop]);
+
+  return (
+    <>
+      <button ref={ref} className="SoundButton" onClick={onClick}>
+        {name}
+        {isPlaying && <animated.div className="fill" style={styles} />}
+      </button>
+    </>
+  );
+};
 
 export default App;
---
+++ src/sounds.js
@@ -0,0 +1,21 @@
+import Ultra from "./samples/UT/ultrakill.wav";
+import Godlike from "./samples/UT/godlike.wav";
+import Dominating from "./samples/UT/dominating.wav";
+import FirstBlood from "./samples/UT/firstblood.wav";
+import DoubleKill from "./samples/UT/doublekill.wav";
+import HeadShot from "./samples/UT/headshot.wav";
+import KillingSpree from "./samples/UT/killingspree.wav";
+import MegaKill from "./samples/UT/megakill.wav";
+import MonsterKill from "./samples/UT/monsterkill.wav";
+
+export const Sounds = [
+  { name: "Ultra kill", src: Ultra },
+  { name: "God like", src: Godlike },
+  { name: "Dominating", src: Dominating },
+  { name: "First blood", src: FirstBlood },
+  { name: "Double kill", src: DoubleKill },
+  { name: "Headshot", src: HeadShot },
+  { name: "Killing spree", src: KillingSpree },
+  { name: "Mega kill", src: MegaKill },
+  { name: "Monster kill", src: MonsterKill },
+];
